6af173e0aebb9faa5995c0bc5c9008447e3ab2eb
Ayako-Yokoe <niluvsuall@gmail.com>
2025-07-14 09:39:55 -0700

    Fix button type

--- src/components/AddToCartButton.jsx
+++ src/components/AddToCartButton.jsx
@@ -3,7 +3,7 @@ import './AddToCartButton.css';
 
 const AddToCartButton = ({ onClick }) => {
   return (
-    <button className='add-btn' onClick={onClick}>
+    <button type='button' className='add-btn' onClick={onClick}>
       <img src='assets/images/icon-add-to-cart.svg' alt='add-to-cart' className='add-btn--cart-icon' />
       <span className='add-btn--title'>Add to Cart</span>
     </button>
